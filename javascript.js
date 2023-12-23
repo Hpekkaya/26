92d6555bd5b856e8baf04946badb54084ae4abb3
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-23 21:23:28 +0300

    Constructor  and Arrow  function repeated

--- javascript.js
+++ javascript.js
@@ -13,5 +13,68 @@
 // console.log(person.description())
 // console.log(person.showName())
 
+// Constructor
+
+// function Person(first, last, age, eye) {
+//   this.firstName = first;
+//   this.lastName = last;
+//   this.age = age;
+//   this.eyeColor = eye;
+//   this.showInfos = function () {
+//     console.log(this.firstName, " ", this.lastName, " ", this.age);
+//   };
+// }
+
+// const person1 = new Person("Ahmet", "Bay", 25, "Brown");
+// person1.showInfos();
+// console.log("Eye-color :", person1.eyeColor);
+// console.log("Constructor :", person1);
+// console.log("Function showInfos ():", person1.showInfos);
+
+// class Person {
+//   constructor(first, last, age, eye) {
+//     this.firstName = first;
+//     this.lastName = last;
+//     this.age = age;
+//     this.eyeColor = eye;
+//   }
+//     showInfos () {
+//       console.log(this.firstName, " ", this.lastName, " ", this.age);
+//     };
+  
+// }
+// const person2 = new Person("Ahmet", "Bay", 25, "Brown");
+// person2.showInfos();
+// console.log("Eye-color :", person2.eyeColor);
+// console.log("Constructor 2 :", person2);
+// console.log("Function showInfos ():", person2.showInfos);
+
+// class Car {
+//     constructor (name, year) {
+//         this.name = name;
+//         this.year = year;
+//     }
+//     age (x) {
+//         return x-this.year
+//     }
+// }
+
+// let date = new Date();
+// let year = date.getFullYear();
+// let myCar = new Car("Tesla", 2020)
+// console.log(myCar.name," ", myCar.year, " ", myCar.age(year))
+
+// Arrow  fuction ( =>)
+// let hello;
+// hello = function() {
+//     return "Hello World"
+// }
+// console.log(hello())
+
+// hello = (value) => "Hello World" + value
+
+
+// console.log(hello(" and Turkey"))
+
 
 
