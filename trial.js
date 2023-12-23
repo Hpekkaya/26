4ca746bc4990525c0573e7c7cf61b7f3cca85187
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-23 22:23:10 +0300

    functions repeated

--- trial.js
+++ trial.js
@@ -1,43 +1,4 @@
-// let sayi = 1;
-// let sayi2 = sayi
-// sayi2++;
-// console.log(sayi, '-', sayi2)
-
-// const person = {
-//     firstName: "ahmet",
-//     lastName: "can"
-// }
-// // (array için de geçerlidir)
-// const x = person ;
-// x.firstName = "mehmet"
-// console.log(person)
-// console.log(x)
-
-// Since the array and objects reference type;
-// changes in its efects each other,
-
-// person = {firstName:"mehmet"}
-
-// const person = {
-//     firstName: "ahmet",
-//     lastName: "can"
-// }
-
-// // delete person.firstName; 
-// // delete person.lastName;
-// console.log(person)
-// console.log(Object.values(person))
-// //  values() returns the values as an array
-// console.log(Object.keys(person))
-// //  keys() returns the properties as an array
-// console.log(Object.entries(person))
-// //  entries() returns the properties as an array
-// console.log(Object.entries(person)[1][1])
-// console.log(Object.entries(person)[1])
 
 
 
 
-
-// let date = new Date()
-// date.getDate
