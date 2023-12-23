4eada25ffcccebfa9e34e6efccffc103b448cf03
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-23 20:42:34 +0300

    this repeated

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// this 
+
+// const person = {
+//     name : "Mustafa",
+//     fname: "Unal",
+//     description : function() {
+//         return this;
+//     },
+//     showName: function() {
+//         return this.name
+//     }
+// }
+// console.log(person.description())
+// console.log(person.showName())
+
+
+
