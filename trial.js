44b0a371859c74201fc5166db4d7805742dac157
Hpekkaya <Ahpek1970@yahoo.com>
2023-12-24 10:06:39 +0300

    rest parameter

--- trial.js
+++ trial.js
@@ -1,4 +1,13 @@
+// rest parameter
+
+// function total(...args) {
+//     let sum = 0;
+//     for (let arg of args){
+//        sum += arg
+//     }
+//     return sum
+// }
 
 
 
-
+// console.log(total(20, 11));
